b9d169e1d7b1079f58de48e8a95e2a76c1d3f142
grimshaw-a <58604535+grimshaw-a@users.noreply.github.com>
2019-12-11 11:51:14 -0700

    Add files via upload

---
+++ Chapter1/AreaPerimeter.java
@@ -0,0 +1,10 @@
+// Dec. 11, 2019
+// CSCI 1105
+// Adam Grimshaw
+
+class RectangleDimensions {
+	public static void main(String[] args) {
+		System.out.println("Area: " + 4.5 * 7.9);
+		System.out.println("Perimeter: " + (4.5 * 2 + 7.9 * 2));
+	}
+}
---
+++ Chapter1/Equation.java
@@ -0,0 +1,9 @@
+// Dec. 11, 2019
+// CSCI 1105
+// Adam Grimshaw
+
+class SimpleEquation {
+	public static void main(String[] args) {
+		System.out.println((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+	}
+}
