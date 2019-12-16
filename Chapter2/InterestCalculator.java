ebadb7ec35da8f7e3f29b7d6f531017fc8986e38
grimshaw-a <58604535+grimshaw-a@users.noreply.github.com>
2019-12-16 09:45:09 -0700

    Add files via upload

---
+++ Chapter2/InterestCalculator.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class InterestCalculator {
+
+	public static void main(String[] args) {
+		final double INTEREST_RATE = 0.05;
+		
+		// create scanner
+		Scanner input = new Scanner(System.in);
+		
+		// prompt user to enter monthly amount
+		System.out.print("Enter monthly savings amount: ");
+		double monthlyAmount = input.nextDouble();
+		
+		// calculate interest
+		double monthlyInterestRate = INTEREST_RATE / 12;
+		double firstMonthTotal = monthlyAmount * (1 + monthlyInterestRate);
+		double secondMonthTotal = (firstMonthTotal + monthlyAmount) * (1 + monthlyInterestRate);
+		double thirdMonthTotal = (secondMonthTotal + monthlyAmount) * (1 + monthlyInterestRate);
+		double fourthMonthTotal = (thirdMonthTotal + monthlyAmount) * (1 + monthlyInterestRate);
+		double fifthMonthTotal = (fourthMonthTotal + monthlyAmount) * (1 + monthlyInterestRate);
+		double sixthMonthTotal = (fifthMonthTotal + monthlyAmount) * (1 + monthlyInterestRate);
+		
+		// display result
+		System.out.println("After the sixth month, the account value is $" + sixthMonthTotal);
+	}
+}
